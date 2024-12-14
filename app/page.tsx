3b30de606367a72a15f27b9d6e039a0b6de0056d
MoshkovSergey <moshkov_s@vk.com>
2024-12-14 12:54:24 +0300

    fix description

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Сохраняйте и мгновенно просматривайте свои изменения.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
